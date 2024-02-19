c302ff06265d1d699e7295c8b58db2d2559714e5
ab192130 <5365476+ab192130@users.noreply.github.com>
2024-02-19 16:38:50 +0400

    minor changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import * as v8 from 'v8';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log('salam');
+    console.log('heap_stats', v8.getHeapStatistics());
+
     return 'Hello World!';
+  }
+
+  postHello(): string {
+    console.log('heap_stats', v8.getHeapStatistics());
+
+    return "What's on your mind?";
   }
 }
